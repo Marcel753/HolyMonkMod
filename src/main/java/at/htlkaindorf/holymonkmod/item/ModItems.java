1047589306c62af2b66963577628b52f4406f6b9
Marcel Fina <marci.fina@gmail.com>
2020-10-27 17:20:00 +0100

    added Neue Kreuzarten

--- src/main/java/at/htlkaindorf/holymonkmod/item/ModItems.java
+++ src/main/java/at/htlkaindorf/holymonkmod/item/ModItems.java
@@ -1,17 +1,22 @@
 package at.htlkaindorf.holymonkmod.item;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems
 {
+    public static ItemBase kreuzHolz = new ItemBase("kreuz_holz").setCreativeTab(CreativeTabs.MATERIALS);
+
     public static void register(IForgeRegistry<Item> registry)
     {
-
+        registry.registerAll(
+                kreuzHolz
+        );
     }
 
     public static void registerModels()
     {
-
+        kreuzHolz.registerItemModel();
     }
 }
--- src/main/java/at/htlkaindorf/holymonkmod/proxy/CommonProxy.java
+++ src/main/java/at/htlkaindorf/holymonkmod/proxy/CommonProxy.java
@@ -1,8 +1,6 @@
 package at.htlkaindorf.holymonkmod.proxy;
 
-import at.htlkaindorf.holymonkmod.item.ItemBase;
 import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
 
 public class CommonProxy
 {
